a39079a523e50f86274089efba4102a2f5a0c061
Justin Makeig <jmpublic+github@makeig.com>
2016-08-17 10:41:36 -0700

    Adds security config to enable use by a non-admin user
    Adds application admin, read, write, internal, and nobody roles.

--- entity-services-examples/src/main/java/com/marklogic/entityservices/examples/CSVLoader.java
+++ entity-services-examples/src/main/java/com/marklogic/entityservices/examples/CSVLoader.java
@@ -31,103 +31,95 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.client.io.StringHandle;
+import com.marklogic.client.io.DocumentMetadataHandle.Capability;
 import com.marklogic.datamovement.JobReport;
 import com.marklogic.datamovement.WriteHostBatcher;
 
 /**
- *  This file is simple an example of how to load CSV files into MarkLogic.
- *  It uses a csvMapper to create JSON documents, one for each row in the CSV.
- *  For EA-3 this is just an example on how to use DMSDK wtih CSV and JSON.
- *  It loads these 'raw' JSON documents for later processing
- *  with in-place transform services. (Post EA-3)
+ * This file is simple an example of how to load CSV files into MarkLogic. It
+ * uses a csvMapper to create JSON documents, one for each row in the CSV. For
+ * EA-3 this is just an example on how to use DMSDK wtih CSV and JSON. It loads
+ * these 'raw' JSON documents for later processing with in-place transform
+ * services. (Post EA-3)
  */
 public class CSVLoader extends ExamplesBase {
 
+	private static Logger logger = LoggerFactory.getLogger(CSVLoader.class);
 
-    private static Logger logger = LoggerFactory.getLogger(CSVLoader.class);
+	private CsvSchema bootstrapSchema;
+	private ObjectMapper csvMapper;
 
+	private CSVLoader() throws IOException {
+		super();
 
-    private CsvSchema bootstrapSchema;
-    private ObjectMapper csvMapper;
+		bootstrapSchema = CsvSchema.emptySchema().withHeader();
+		csvMapper = new CsvMapper();
 
-    private CSVLoader() throws IOException {
-        super();
+	}
 
-        bootstrapSchema = CsvSchema.emptySchema().withHeader();
-        csvMapper = new CsvMapper();
+	private void go() throws InterruptedException {
 
+		logger.info("job started.");
 
-    }
+		File dir = new File(props.getProperty("referenceDataDir") + "/csv");
 
-    private void go() throws InterruptedException {
+		WriteHostBatcher batcher = moveMgr.newWriteHostBatcher().withBatchSize(100).withThreadCount(10)
+				.onBatchSuccess((client, batch) -> logger.info(getSummaryReport()))
+				.onBatchFailure((client, batch, throwable) -> {
+					logger.warn("FAILURE on batch:" + batch.toString() + "\n", throwable);
+					throwable.printStackTrace();
+				});
 
+		ticket = moveMgr.startJob(batcher);
 
-        logger.info("job started.");
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir.toPath(), "*.csv")) {
+			for (Path entry : stream) {
+				logger.info("Adding " + entry.getFileName().toString());
 
-        File dir = new File(props.getProperty("referenceDataDir") + "/csv");
+				MappingIterator<ObjectNode> it = csvMapper.readerFor(ObjectNode.class).with(bootstrapSchema)
+						.readValues(entry.toFile());
+				long i = 0;
+				while (it.hasNext()) {
+					ObjectNode jsonNode = it.next();
+					String jsonString = mapper.writeValueAsString(jsonNode);
 
-        WriteHostBatcher batcher = moveMgr.newWriteHostBatcher()
-                .withBatchSize(100)
-                .withThreadCount(10)
-                .onBatchSuccess( (client, batch) ->  logger.info(getSummaryReport()) )
-                .onBatchFailure(
-                        (client, batch, throwable) -> {
-                            logger.warn("FAILURE on batch:" + batch.toString() + "\n",
-                                    throwable);
-                            throwable.printStackTrace();
-                        }
-                );
+					String uri = entry.getFileName().toString() + "-" + Long.toString(i++) + ".json";
+					DocumentMetadataHandle metadata = new DocumentMetadataHandle() //
+							.withCollections("raw", "csv") //
+							.withPermission("race-reader", Capability.READ) //
+							.withPermission("race-writer", Capability.INSERT, Capability.UPDATE);
+					batcher.add(uri, metadata, new StringHandle(jsonString));
+					if (i % 1000 == 0)
+						logger.info("Inserting JSON document " + uri);
+				}
+				it.close();
+			}
+		}
 
-        ticket = moveMgr.startJob(batcher);
+		catch (IOException e)
 
-        try( DirectoryStream<Path> stream = Files.newDirectoryStream(dir.toPath(), "*.csv") )
-        {
-            for (Path entry : stream) {
-                logger.info("Adding " + entry.getFileName().toString());
+		{
+			e.printStackTrace();
+		}
 
-                MappingIterator<ObjectNode> it = csvMapper.readerFor(ObjectNode.class)
-                        .with(bootstrapSchema)
-                        .readValues(entry.toFile());
-                long i=0;
-                while (it.hasNext()) {
-                    ObjectNode jsonNode = it.next();
-                    String jsonString = mapper.writeValueAsString(jsonNode);
+		batcher.flush();
+	}
 
-                    String uri = entry.getFileName().toString() + "-" + Long.toString(i++) + ".json";
-                    DocumentMetadataHandle metadata = new DocumentMetadataHandle().withCollections("raw", "csv");
-                    batcher.add(uri, metadata, new StringHandle(jsonString));
-                    if (i % 1000 == 0) logger.info("Inserting JSON document " + uri);
-                }
-                it.close();
-            }
-        }
+	private String getSummaryReport() {
+		JobReport report = moveMgr.getJobReport(ticket);
+		if (report == null) {
+			// is this a bug or not implemented TODO
+			return "Report is null";
+		} else {
+			return "batches: " + report.getSuccessBatchesCount() + ", bytes: " + report.getBytesMoved() + ", failures: "
+					+ report.getFailureBatchesCount();
+		}
+	}
 
-        catch( IOException e )
+	public static void main(String[] args) throws Exception {
 
-        {
-            e.printStackTrace();
-        }
+		CSVLoader integrator = new CSVLoader();
+		integrator.go();
 
-        batcher.flush();
-    }
-
-    private String getSummaryReport() {
-        JobReport report = moveMgr.getJobReport(ticket);
-        if (report == null) {
-            // is this a bug or not implemented TODO
-            return "Report is null";
-        }
-        else {
-            return "batches: " + report.getSuccessBatchesCount() +
-                    ", bytes: " + report.getBytesMoved() +
-                    ", failures: " + report.getFailureBatchesCount();
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        CSVLoader integrator = new CSVLoader();
-        integrator.go();
-
-    }
+	}
 }
--- entity-services-examples/src/main/java/com/marklogic/entityservices/examples/ExamplesBase.java
+++ entity-services-examples/src/main/java/com/marklogic/entityservices/examples/ExamplesBase.java
@@ -20,6 +20,7 @@ import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.DatabaseClientFactory;
 import com.marklogic.client.document.ServerTransform;
 import com.marklogic.client.io.DocumentMetadataHandle;
+import com.marklogic.client.io.DocumentMetadataHandle.Capability;
 import com.marklogic.client.io.FileHandle;
 import com.marklogic.client.io.Format;
 import com.marklogic.datamovement.DataMovementManager;
@@ -35,116 +36,101 @@ import java.nio.file.Path;
 import java.util.Properties;
 
 /**
- * Base class for examples.
- * See the importJSON method for generic loading of JSON from a directory tree.
+ * Base class for examples. See the importJSON method for generic loading of
+ * JSON from a directory tree.
  */
 abstract class ExamplesBase {
 
-    private static Logger logger = LoggerFactory.getLogger(ExamplesBase.class);
+	private static Logger logger = LoggerFactory.getLogger(ExamplesBase.class);
 
-    protected DataMovementManager moveMgr;
-    protected JobTicket ticket;
-    protected ObjectMapper mapper;
-    protected Properties props;
+	protected DataMovementManager moveMgr;
+	protected JobTicket ticket;
+	protected ObjectMapper mapper;
+	protected Properties props;
 
-    public ExamplesBase() throws IOException {
-        props = new Properties();
-        props.load(this.getClass().getClassLoader().getResourceAsStream("application.properties"));
+	public ExamplesBase() throws IOException {
+		props = new Properties();
+		props.load(this.getClass().getClassLoader().getResourceAsStream("application.properties"));
 
-        DatabaseClient client = DatabaseClientFactory.newClient(
-                props.getProperty("mlHost"),
-                Integer.parseInt(props.getProperty("mlRestPort")),
-                new DatabaseClientFactory.DigestAuthContext(
-                    props.getProperty("mlUsername"),
-                    props.getProperty("mlPassword")));
+		DatabaseClient client = DatabaseClientFactory.newClient(props.getProperty("mlHost"),
+				Integer.parseInt(props.getProperty("mlRestPort")), new DatabaseClientFactory.DigestAuthContext(
+						props.getProperty("mlUsername"), props.getProperty("mlPassword")));
 
-        moveMgr = DataMovementManager.newInstance().withClient(client);
-        mapper = new ObjectMapper();
-    }
+		moveMgr = DataMovementManager.newInstance().withClient(client);
+		mapper = new ObjectMapper();
+	}
 
-    private void importOrDescend(Path directory, WriteHostBatcher batcher, String collection, Format format) {
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
-            for (Path entry : stream) {
-                if (entry.toFile().isDirectory()) {
-                    logger.info("Reading subdirectory " + entry.getFileName().toString());
-                    importOrDescend(entry, batcher, collection, format);
-                } else {
-                    logger.info("Adding " + entry.getFileName().toString());
-                    String uri = entry.toString();
-                    if (collection != null) {
-                        DocumentMetadataHandle metadata = new DocumentMetadataHandle().withCollections(collection);
-                        batcher.add(uri, metadata, new FileHandle(entry.toFile()).withFormat(format));
-                    }
-                    else {
-                        batcher.add(uri, new FileHandle(entry.toFile()).withFormat(format));
-                    }
-                    logger.info("Inserted " + format.toString() + " document " + uri);
-                }
-            }
+	private void importOrDescend(Path directory, WriteHostBatcher batcher, String collection, Format format) {
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+			for (Path entry : stream) {
+				if (entry.toFile().isDirectory()) {
+					logger.info("Reading subdirectory " + entry.getFileName().toString());
+					importOrDescend(entry, batcher, collection, format);
+				} else {
+					logger.info("Adding " + entry.getFileName().toString());
+					String uri = entry.toString();
+					if (collection != null) {
+						DocumentMetadataHandle metadata = new DocumentMetadataHandle().withCollections(collection) //
+								.withPermission("race-reader", Capability.READ) //
+								.withPermission("race-writer", Capability.INSERT, Capability.UPDATE);
+						batcher.add(uri, metadata, new FileHandle(entry.toFile()).withFormat(format));
+					} else {
+						batcher.add(uri, new FileHandle(entry.toFile()).withFormat(format));
+					}
+					logger.info("Inserted " + format.toString() + " document " + uri);
+				}
+			}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /*
-      This method uses a document-centric approach to RDF reference data, by invoking
-      a server-side transform that parses turtle into MarkLogic XML triples.
-     */
-    public void importRDF(Path referenceDataDir, String collection) {
+	/*
+	 * This method uses a document-centric approach to RDF reference data, by
+	 * invoking a server-side transform that parses turtle into MarkLogic XML
+	 * triples.
+	 */
+	public void importRDF(Path referenceDataDir, String collection) {
 
-        logger.info("RDF Load Job started");
+		logger.info("RDF Load Job started");
 
-        WriteHostBatcher batcher = moveMgr.newWriteHostBatcher()
-                .withBatchSize(1)
-                .withThreadCount(1)
-                .withTransform(new ServerTransform("turtleToXml"))
-                .onBatchSuccess( (client, batch) ->  logger.info("Loaded rdf data batch") )
-                .onBatchFailure(
-                        (client, batch, throwable) -> {
-                            logger.warn("FAILURE on batch:" + batch.toString() + "\n",
-                                    throwable);
-                            throwable.printStackTrace();
-                        }
-                );
-        ;
-        ticket=moveMgr.startJob(batcher);
+		WriteHostBatcher batcher = moveMgr.newWriteHostBatcher().withBatchSize(1).withThreadCount(1)
+				.withTransform(new ServerTransform("turtleToXml"))
+				.onBatchSuccess((client, batch) -> logger.info("Loaded rdf data batch"))
+				.onBatchFailure((client, batch, throwable) -> {
+					logger.warn("FAILURE on batch:" + batch.toString() + "\n", throwable);
+					throwable.printStackTrace();
+				});
+		;
+		ticket = moveMgr.startJob(batcher);
 
-        importOrDescend(referenceDataDir, batcher, collection, Format.TEXT);
+		importOrDescend(referenceDataDir, batcher, collection, Format.TEXT);
 
-        batcher.flush();
+		batcher.flush();
 
-    }
+	}
 
-    public void importJSON(Path jsonDirectory) throws InterruptedException, IOException {
-       importJSON(jsonDirectory, null);
-    }
+	public void importJSON(Path jsonDirectory) throws InterruptedException, IOException {
+		importJSON(jsonDirectory, null);
+	}
 
-    public void importJSON(Path jsonDirectory, String toCollection) throws IOException {
+	public void importJSON(Path jsonDirectory, String toCollection) throws IOException {
 
-        logger.info("job started.");
+		logger.info("job started.");
 
-        WriteHostBatcher batcher = moveMgr.newWriteHostBatcher()
-                .withBatchSize(10)
-                .withThreadCount(2)
-                .onBatchSuccess( (client, batch) ->  logger.info("Loaded entity types batch") )
-                .onBatchFailure(
-                        (client, batch, throwable) -> {
-                            logger.warn("FAILURE on batch:" + batch.toString() + "\n",
-                                    throwable);
-                            throwable.printStackTrace();
-                        }
-                );
+		WriteHostBatcher batcher = moveMgr.newWriteHostBatcher().withBatchSize(10).withThreadCount(2)
+				.onBatchSuccess((client, batch) -> logger.info("Loaded entity types batch"))
+				.onBatchFailure((client, batch, throwable) -> {
+					logger.warn("FAILURE on batch:" + batch.toString() + "\n", throwable);
+					throwable.printStackTrace();
+				});
 
-        ticket = moveMgr.startJob(batcher);
+		ticket = moveMgr.startJob(batcher);
 
-        importOrDescend(jsonDirectory, batcher, toCollection, Format.JSON);
+		importOrDescend(jsonDirectory, batcher, toCollection, Format.JSON);
 
-        batcher.flush();
-    }
-
+		batcher.flush();
+	}
 
 }
-
-
-
